26d5871cabb9a4dff4449b858ec372215003dc4d
Petr <petr.posvic@volny.cz>
2022-03-21 13:09:43 +0100

    Update TouchCalibration.ino

--- examples/TouchCalibration/TouchCalibration.ino
+++ examples/TouchCalibration/TouchCalibration.ino
@@ -1,17 +1,34 @@
 // ESP32 Touch Calibration
 
 #define touch_pin 13  // 13 for Heltec, 13 for LoLin
-#define touch_pin2 14  // 14 for Heltec, 14 for LoLin
+
+int minimum = 255;
+int maximum = 0;
+
+int touchValue;
 
 void setup() {
   Serial.begin(115200);
   delay(1000); // give me time to bring up serial monitor
-  Serial.println("ESP32 Touch Test");
+  Serial.println("ESP32 Touch Calibration");
+
+  Serial.print("touchValue");       // Start serial plotter
+  Serial.print("\t");
+  Serial.print("minimum");
+  Serial.print("\t");
+  Serial.println("maximum");
+
 }
 
 void loop() {
-  Serial.print(touchRead(touch_pin));       // Start serial plotter 
+  touchValue = touchRead(touch_pin);
+  if (touchValue > maximum) maximum = touchValue;
+  if (touchValue < minimum) minimum = touchValue;
+  
+  Serial.print(touchValue);       // Start serial plotter
   Serial.print("\t");
-  Serial.println(touchRead(touch_pin2));
+  Serial.print(minimum);
+  Serial.print("\t");
+  Serial.println(maximum);
   delay(10);
 }
